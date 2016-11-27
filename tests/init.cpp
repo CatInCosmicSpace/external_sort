97ea5cec0cc6cc205a75ceb6ce83710e0cac74eb
Alena Egorova <CatInCosmicSpace@users.noreply.github.com>
2016-11-28 00:19:02 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -26,18 +26,20 @@ SCENARIO("file - 8 mb, memory - 1 mb", "[8 - 1]") {
 SCENARIO("file - 15 mb, memory - 4 mb", "[15 - 4]") {
 	external_sort("15", "sorted15", 4 * 1024 * 1024);
 
-	std::ifstream output("sorted15");
+	std::ifstream output_first("sorted15"), output_second("sorted15");
 	data first, second;
 	bool flag = true;
-	output >> first.last_name >> first.first_name >> first.year;
-	while (output) {
-		output >> second.last_name >> second.first_name >> second.year;
-		if (!output) break;
-		if (!(first < second)) {
+	output_second >> first.last_name >> first.first_name >> first.year;
+	while (output_second) {
+		output_first >> first.last_name >> first.first_name >> first.year;
+		output_second >> second.last_name >> second.first_name >> second.year;
+		if (!output_second) break;
+		if (!(first <= second)) {
+			output_first.close();
+			output_second.close();
 			flag = false;
 			break;
 		}
-		first = second;
 	}
 
 	REQUIRE(flag);
@@ -46,18 +48,20 @@ SCENARIO("file - 15 mb, memory - 4 mb", "[15 - 4]") {
 SCENARIO("file - 32 mb, memory - 17 mb", "[32 - 17]") {
 	external_sort("15", "sorted15", 17 * 1024 * 1024);
 
-	std::ifstream output("sorted32");
+	std::ifstream output_first("sorted32"), output_second("sorted32");
 	data first, second;
 	bool flag = true;
-	output >> first.last_name >> first.first_name >> first.year;
-	while (output) {
-		output >> second.last_name >> second.first_name >> second.year;
-		if (!output) break;
-		if (!(first < second)) {
+	output_second >> first.last_name >> first.first_name >> first.year;
+	while (output_second) {
+		output_first >> first.last_name >> first.first_name >> first.year;
+		output_second >> second.last_name >> second.first_name >> second.year;
+		if (!output_second) break;
+		if (!(first <= second)) {
+			output_first.close();
+			output_second.close();
 			flag = false;
 			break;
 		}
-		first = second;
 	}
 
 	REQUIRE(flag);
